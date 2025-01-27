7099a8d5c7976ad1dbe20ab270ea21b06221bfd6
Anima <cjaccardh@gmail.com>
2025-01-27 18:38:12 -0300

    Lo tuve que gepetiar un poquito, pero igual logré llegar a la solución. App base de FCC ampliada

--- JavaScript Fundamentals/FreeCodeCamp Projects/1. pyramid.js
+++ JavaScript Fundamentals/FreeCodeCamp Projects/1. pyramid.js
@@ -3,19 +3,49 @@
 const character = "#";
 const count = 8;
 const rows = [];
-let inverted = true;
+let inverted = 4;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+
+function padRowSide(rowNumber) {
+  return character.repeat(rowNumber - 1);
+}
+
+function padRowSideOther(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber - 1);
+}
+
 for (let i = 1; i <= count; i++) {
-  if (inverted) {
+  if (inverted === 1) {
     rows.unshift(padRow(i, count));
-  } else {
+  } else if (inverted === 2) {
     rows.push(padRow(i, count));
+  } else if (inverted === 3) {
+    if (i <= count + 1) {
+      rows.push(padRowSide(i));
+    }
+    if (i === count) {
+      for (let j = count - 1; j > 0; j--) {
+        rows.push(padRowSide(j));
+      }
+    }
+  } else {
+    if (i <= count) {
+      // Para la parte de la pirámide apuntando a la izquierda
+      rows.push(padRowSideOther(i, count)); // Primero creamos las filas crecientes
+    }
+    if (i === count) {
+      // Luego decrecemos las filas para simular la pirámide
+      for (let j = count - 1; j > 0; j--) {
+        rows.push(padRowSideOther(j, count)); // Filas decrecientes
+      }
+    }
   }
 }
+// Store the result
 
 let result = ""
 
